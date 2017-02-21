f2360dc58beb0d0ae60d63face47b3bdd45d38f6
Ross Brazuk <ross.brazuk@gmail.com>
2017-02-21 13:33:09 -0500

    added Game and Play models

---
+++ app/src/main/java/com/rbrazuk/ross/tabletop_tracker/Game.java
@@ -0,0 +1,28 @@
+package com.rbrazuk.ross.tabletop_tracker;
+
+import java.util.List;
+
+public class Game {
+
+    private String mTitle;
+    private List<Play> mPlays;
+
+    public Game() {
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public List<Play> getPlays() {
+        return mPlays;
+    }
+
+    public void setPlays(List<Play> plays) {
+        mPlays = plays;
+    }
+}
---
+++ app/src/main/java/com/rbrazuk/ross/tabletop_tracker/Play.java
@@ -0,0 +1,40 @@
+package com.rbrazuk.ross.tabletop_tracker;
+
+import java.util.Date;
+
+/**
+ * Created by Ross on 2/21/2017.
+ */
+
+public class Play {
+
+    private Game mGame;
+    private Date mDate;
+    private String[] mPlayers;
+
+    public Play() {}
+
+    public Game getGame() {
+        return mGame;
+    }
+
+    public void setGame(Game game) {
+        mGame = game;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public String[] getPlayers() {
+        return mPlayers;
+    }
+
+    public void setPlayers(String[] players) {
+        mPlayers = players;
+    }
+}
